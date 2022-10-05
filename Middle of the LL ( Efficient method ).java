f326d8bf5683e538c95bb04c7aad31cf91a74488
Sujal Sharma <110303512+Sujal-3012@users.noreply.github.com>
2022-10-05 17:03:16 +0530

    Create Middle of the LL ( Efficient method ).java
    The time in which slow pointer reaches the middle of LL , fast pointer will reach end of the LL in same time .
    Hence when fast pointer reaches end , return s as it will be pointing to the middle .

---
+++ Middle of the LL ( Efficient method ).java
@@ -0,0 +1,12 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode s = head;
+        ListNode f = head;
+        
+        while (f != null && f.next != null){
+            s = s.next;
+            f = f.next.next;
+        }
+        return s;
+    }
+}
